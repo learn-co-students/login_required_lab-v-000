6ce16b96d1a2d2cf99e1dda6920f2262ee336a81
dannyd4315 <dannyd4315@gmail.com>
2016-02-19 11:18:24 +1100

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+
+  def new
+  end  
+
+  def create
+    if current_user
+      redirect_to secrets_path
+    else
+      session[:name] = params[:name]
+      redirect_to new_session_path
+    end    
+  end 
+
+  def destroy
+    session.clear if current_user
+    redirect_to new_session_path   
+  end 
+end  
