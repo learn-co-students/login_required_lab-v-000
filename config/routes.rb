07e2f3dd527dc610ab9da7483962769cc5f3e04a
Chen Yang <yangc5@mail.uc.edu>
2016-02-24 22:26:02 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  get 'secret' => 'secrets#show'
+  resources :sessions, only: [:new, :create, :index]
+  post 'sessions/destroy' => 'sessions#destroy'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
