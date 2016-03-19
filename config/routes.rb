9a0f48fda2c9481dbbb0b22e0e2e784eea297571
alexbarron <alexbarron@gmail.com>
2016-03-19 15:21:42 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  get '/secret', to: 'secrets#show'
+  post '/destroy', to: 'sessions#destroy'
+  resources :sessions, only: [:new, :create]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
