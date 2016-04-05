616e8e534c3941a40a02b2dd84eb8294974d5555
pickledyamsman <pickledyamsman@gmail.com>
2016-04-05 15:13:55 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    if params[:name].blank?
+      redirect_to '/new'
+    else
+      session[:name] = params[:name]
+      redirect_to secrets_path
+    end
+  end
+
+  def destroy
+    if session[:name]
+      session.destroy
+    end
+  end
+end
