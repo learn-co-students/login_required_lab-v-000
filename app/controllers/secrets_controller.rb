7b3ca1d63493430f97ff1f5b41631098ff2434e4
Smulligan85 <sean.mulligan85@gmail.com>
2016-04-09 21:11:46 -0700

    Done

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,17 @@
+class SecretsController < ApplicationController
+  before_action :require_login
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  private
+
+  def require_login
+    redirect_to new_session_path unless current_user
+  end
+end
