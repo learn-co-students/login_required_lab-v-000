32d04c3fedab5305f21678e71bd45d02754323ce
Brad1974 <danieltbrad@gmail.com>
2016-04-13 14:03:08 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def index
+  end
+
+  def create
+    if current_user
+      redirect_to sessions_path
+    else
+      session[:name] = params[:name]
+      redirect_to new_session_path
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    session[:name] = nil
+    redirect_to new_session_path
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :sessions
+  get 'sessions/destroy', to: "sessions#destroy"
+  get 'secrets', to: 'secrets#show'
+
+#  root 'sessions#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
