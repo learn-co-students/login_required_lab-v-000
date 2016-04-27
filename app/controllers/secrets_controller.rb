b2c7ee8eef3a507c4df4cce34fa15623b2b32cf6
agdavid <antonio.g.david@gmail.com>
2016-04-27 12:19:56 -0400

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,9 +1,13 @@
 class SecretsController < ApplicationController
+  before_action :logged_in
+
   def show
-    if current_user
-      render "show"
-    else
-      redirect_to "/"
-    end
+    #tests if #current_user (i.e., session[:name] has value)
+    #renders secrets/show.html.erb
   end
+
+  private
+    def logged_in
+      redirect_to "/" unless current_user
+    end
 end
