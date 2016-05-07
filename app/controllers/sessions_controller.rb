2e388af343977f3c8d43d02345e897ccf1a56d36
glee38 <glee3388@gmail.com>
2016-05-07 18:39:25 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def index
+    @current_user = current_user
+  end
+
+  def create
+    if params[:name].present?
+      session[:name] = params[:name]
+      redirect_to sessions_path
+    else
+      redirect_to new_session_path
+    end
+  end
+
+  def destroy
+    if session[:name]
+      session.clear
+      redirect_to sessions_path
+    else
+      redirect_to sessions_path
+    end
+  end
+end
