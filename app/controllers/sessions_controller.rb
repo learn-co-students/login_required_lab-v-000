750ce55fffcb007520ba888307fa87607c351827
thebrianemory <the@brianemory.com>
2016-06-12 11:06:36 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+  def login
+
+  end
+
+  def new
+
+  end
+
+  def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to '/login'
+    else
+      session[:name] = params[:name]
+      redirect_to '/secrets'
+    end
+  end
+
+  def destroy
+    if session[:name].nil?
+    else
+      reset_session
+    end
+    redirect_to '/login'
+  end
+end
