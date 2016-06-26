8a4a316a27248f2bd26eed2f3f0c58b1008fb9fc
PeteAJ <peter.jacobson@gmail.com>
2016-06-26 12:30:27 -0400

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,14 +1,6 @@
 class SecretsController < ActionController::Base
-before_action :logged_in
-
-
-  def show
+before_action :require_logged_in
   
+  def show
   end
-
-  private
-    def logged_in
-      redirect_to "/" unless current_user
-    end
-
 end
