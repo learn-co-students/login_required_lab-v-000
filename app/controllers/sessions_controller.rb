f059295a313736ad2b7b0d13a0618f3cfcb3c93f
Andrew Papacostas <a_papacostas@yahoo.com>
2016-08-02 14:11:51 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  before_action :require_login
+
+  
+
+def new
+end
+
+def create
+  if params[:name].nil? || params[:name].empty?
+    redirect_to new_sessions_path
+  else
+    session[:name] = params[:name]
+    redirect_to sessions_path
+  end
+end
+
+def destroy
+  if current_user.nil?
+    redirect_to new_sessions_path
+  else
+    session[:name] = nil
+    redirect_to new_sessions_path
+  end
+end
+
+end
