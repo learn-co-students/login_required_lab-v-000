01f8fd86454443d34cee96ef635572415533c44b
m-walker <michaelwalker610@gmail.com>
2016-08-23 15:14:06 -0400

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,8 @@
+class SecretsController < ApplicationController
+
+  def show
+    redirect_to new_session_path if !current_user
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'secret', to: 'secrets#show'
+  post 'session', to: 'sessions#destroy'
+  resources :sessions, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
