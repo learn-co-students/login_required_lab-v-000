83c050aae9d896a64aaca8aaaefd5dac424ad2bc
Motti Mordechay Gottlieb <mottigottlieb@gmail.com>
2016-10-19 13:30:57 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
+     get '/' => 'sessions#new'
+     get 'see' => 'application#see'
+     post '/login' => 'sessions#create'
+#        No route matches {:action=>"destroy", :controller=>"sessions"}
+    post '/destroy' => 'sessions#destroy'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
