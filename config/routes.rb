9bdb5b7d4a13a87692a3049493b9e22adbf95cd6
Rebecca <beckysherman000@gmail.com>
2016-10-28 16:03:55 -0400

    Fix logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root    'sessions#new'
   post    '/sessions',  to: 'sessions#create'
-  post    '/logout', to: 'sessions#destroy'
+  delete    '/logout', to: 'sessions#destroy'
   get     '/secrets', to: 'secrets#show'
 end
