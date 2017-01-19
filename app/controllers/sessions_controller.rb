46fbcbb0c9b4f09c81cf5c0a49faf9c088f0736b
Bill <storywr@gmail.com>
2017-01-19 18:25:29 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+  before_action :require_login
+  skip_before_action :require_login, only: [:new]
+
+    def login
+    end
+
+    def new
+    end
+
+    def create
+      session[:name] = params[:name]
+    end
+
+    def destroy
+      session.delete :name
+    end
+
+    private
+
+    def require_login
+      redirect_to controller: 'sessions', action: 'new' unless session.include? :user_id
+    end
+
+
+end
