4d399f4ef86ba3106a1240ee71382ef9455e63d4
Bill <storywr@gmail.com>
2017-01-19 19:58:12 -0500

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,14 +1,6 @@
 class SecretsController < ApplicationController
 
-  before_action :require_login
-
     def show
-    end
-
-    private
-
-    def require_login
-      redirect_to controller: 'sessions', action: 'new' unless session.include? :user_id
     end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,6 @@
 class SessionsController < ApplicationController
-
-  before_action :require_login
   skip_before_action :require_login, only: [:new]
   skip_before_filter :verify_authenticity_token, :only => :create
-
-    def login
-    end
 
     def new
     end
@@ -17,12 +12,6 @@ class SessionsController < ApplicationController
 
     def destroy
       session.delete :name
-    end
-
-    private
-
-    def require_login
-      redirect_to controller: 'sessions', action: 'new' unless session.include? :user_id
     end
 
 end
