5f315bae0b4fc614e4a9cb713dc468b4b0e17d25
neener <janinemrosen@gmail.com>
2017-01-19 23:05:59 -0500

    redo

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,12 +2,8 @@ class SecretsController < ApplicationController
 	before_action :require_login
 
 	def show
-
+		
 	end
 
-	private
 
-	def require_login
-		redirect_to controller: 'sessions', action: 'new' unless current_user
-	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
 
+	def new
+	end
+
 	def create
-		if params[:name].nil? || params[:name].empty?
-			redirect_to '/login'
-		else
-			session[:name] = params[:name]
-			redirect_to '/login'
-		end
+		return redirect_to(controller: 'sesions', action: 'new') if !params[:name] || params[:name].empty?
+		session[:name] = params[:name]
+		redirect_to controller: 'application', action: 'hello'
 	end
 
 	def destroy
 		session.delete :name
-		redirect_to '/login'
+		redirect_to controller: 'application', action: 'hello'
 	end
 end
