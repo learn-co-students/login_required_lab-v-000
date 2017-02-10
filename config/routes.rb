499434e72169dd74e507d370c88ce2023337a778
WilliamMena <williammena1991@gmail.com>
2017-02-10 00:12:44 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root 'sessions#home'
+  get '/login' => 'sessions#new'
+  post '/create' => 'sessions#create'
+  post '/destroy' => 'sessions#destroy'
+
+  get '/secretsss' => 'secrets#show'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
