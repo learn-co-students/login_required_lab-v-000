0d31933d7907219f3b98b8d922bfa726e766079f
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-03-15 10:23:33 +0000

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,9 +1,20 @@
 class SecretsController < ApplicationController
+   before_action :require_login
+   skip_before_action :require_login, only: [:show]
+
 
   def show
     if !logged_in?
       redirect_to sessions_new_path
+    else
+      render :show
     end
   end
+
+  private
+
+  def require_login
+   return head(:forbidden) unless session.include? :user_id
+ end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,28 @@
 class SessionsController < ApplicationController
+  # before_action :require_login
+  # skip_before_action :require_login, only: [:new]
 
   def new
-  end 
+  end
+
+  def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to sessions_new_path
+    else
+      session[:name] = params[:name]
+      render 'secrets/show'
+    end
+  end
+
+  def destroy
+    session[:name] ? session.clear : session[:name] = nil
+    redirect_to sessions_new_path
+  end
+
+  private
+
+  def require_login
+   return head(:forbidden) unless session.include? :user_id
+ end
+
 end
