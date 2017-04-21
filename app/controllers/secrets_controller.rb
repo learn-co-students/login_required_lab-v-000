aa3a52503b94312801f9687233da75cba414a6e9
vanitanaidu <vanita112003@gmail.com>
2017-04-21 19:02:50 -0400

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,11 @@
+class SecretsController < ApplicationController
+  before_action :require_login
+
+  def show
+    
+  end
+
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+
+end
