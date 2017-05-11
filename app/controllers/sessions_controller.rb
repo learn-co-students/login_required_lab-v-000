a5d88f6f39d38154e9f372c8af4f8db2083ee638
Howard DeVennish <howarddevennish@gmail.com>
2017-05-11 02:17:04 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,25 @@
 class SessionsController < ApplicationController
 
+  def new
+    render :login
+  end
+
+  def create
+    if params[:name] != "" && !params[:name].nil?
+      current_user = params[:name]
+      session[:name] = current_user
+      @name = current_user
+      render :show
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    if !session[:name].nil?
+      session.clear
+    end
+    redirect_to login_path
+  end
+
 end
