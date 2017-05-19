878cb2d5593b1aaa1d2cdf15295b50bdede6100d
github username apalski <github email address annette.drapalski@gmail.com>
2017-05-19 19:40:22 +1000

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,13 @@
+
+class SecretsController < ApplicationController
+
+	def show
+		if current_user
+			render secrets_show_path
+		else
+			redirect_to sessions_new_path
+		end		
+	end
+
+	private
+end
