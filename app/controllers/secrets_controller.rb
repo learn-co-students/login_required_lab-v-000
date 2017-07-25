d2676293cf600bddff493806814743762838aef0
benhayehudi <bengreenberg@gmail.com>
2017-07-25 12:05:27 -0400

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,16 @@
+class SecretsController < ApplicationController
+  before_action :require_login
+
+  def hello
+    @user = current_user
+  end
+
+  def show
+  end
+
+  private
+
+  def require_login
+    redirect_to root_path unless current_user
+  end
+end
