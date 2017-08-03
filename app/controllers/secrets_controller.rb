c6f7994d54831650abfdfa470eca6208b8c47b8b
sarjumulmi <mulmi.sarju@gmail.com>
2017-08-03 14:34:49 -0400

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,15 @@
+class SecretsController < ApplicationController
+before_action :require_login
+
+def show
+
+
+end
+
+
+private
+def require_login
+  return redirect_to controller: 'sessions', action: 'new' unless session.include? :name
+end
+
+end
