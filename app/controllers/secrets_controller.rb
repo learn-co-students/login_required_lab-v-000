5ace4b62024847c0d27d97ab952361013eba10bb
jinstrider2000 <efrainperezjr@live.com>
2017-08-11 00:45:27 -0400

    implemented before_action

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,5 +1,12 @@
 class SecretsController < ApplicationController
+  before_action :redirect_if_logged_out
+
   def show
+    
+  end
+
+  private
+  def redirect_if_logged_out
     redirect_to '/' if current_user.blank?
   end
 end
