951b57460958382f7958820a68274e967f592160
Dustin Anderson <dandersonsoftware@outlook.com>
2017-08-15 12:07:49 -0400

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,17 @@
+class SecretsController < ApplicationController
+  before_action :require_login
+
+  def welcome
+
+  end
+
+  def show
+  end
+
+  private
+
+  def require_login
+    redirect_to root_path unless current_user
+  end
+
+end
