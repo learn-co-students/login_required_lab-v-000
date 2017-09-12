1f2c096c0e8c9d756c8fb0013cfcc435ec2d8d3a
gb23 <gregbenj@hotmail.com>
2017-09-12 16:10:26 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,36 @@
 class SessionsController < ApplicationController
     before_action :require_login
-    
+    skip_before_action :require_login, only: [:destroy]
     def create
-byebug        
+        session[:name] = params[:name]
+        redirect_to root_path
+    end
+
+    def destroy
+        if current_user
+            session.delete :name
+        end
+        redirect_to root_path
     end
 
     private
 
         def require_login
-
-            redirect_to login_path unless current_user
-
+            if !params[:name] || params[:name].empty?
+                redirect_to login_path 
+            end
         end
 end
+
+
+# def create
+#     return redirect_to(controller: 'sessions',
+#                        action: 'new') if !params[:name] || params[:name].empty?
+#     session[:name] = params[:name]
+#     redirect_to controller: 'application', action: 'hello'
+#   end
+
+#   def destroy
+#     session.delete :name
+#     redirect_to controller: 'application', action: 'hello'
+#   end
