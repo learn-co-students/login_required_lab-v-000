e3ca133e5eadbb78ac8b0a70b017881444168164
Aimee Carney <acarney29@gmail.com>
2017-09-23 19:58:33 +0000

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,16 @@
+class SecretsController < ApplicationController
+  before_action :require_login
+
+  def show
+
+  end
+
+  private
+
+  def require_login
+    if session[:name] == "" || session[:name] == nil
+      redirect_to sessions_new_path
+    end
+  end
+
+end
