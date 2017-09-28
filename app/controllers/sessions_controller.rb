676826c08b5f81b2ad54208144a1918be29bf71d
Jack <jackexyou@gmail.com>
2017-09-28 01:04:15 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    session[:name] = params[:name]
+    if session[:name].nil? || session[:name].empty?
+      redirect_to '/'
+    else
+
+      redirect_to '/sessions/index'
+    end
+  end
+
+  def index
+    @name = current_user
+  end
+
+  def destroy
+    session.delete :name if session[:name]
+    redirect_to '/'
+  end
+end 
