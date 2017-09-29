35e8b882c5139762e8b4e2f83b61a5e878b8be6c
psnider30 <pbsnider@gmail.com>
2017-09-28 17:24:57 -0700

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,13 +1,8 @@
 class SecretsController < ApplicationController
 
-before_action :current_user
+before_action :require_logged_in
 
   def show
-    if current_user
-      @user = current_user
-      render :show
-    else
-      redirect_to login_path
-    end
   end
+
 end
