de9d05ed282f5680cc9cc88c8baad0f083e19793
Allen Anderson <dande313@byu.net>
2017-10-06 17:32:05 +0000

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,20 @@
+class SecretsController < ApplicationController
+  before_action :require_login
+
+  def new
+  end
+
+  def show
+  end
+
+
+  private
+
+    def require_login
+      (redirect_to controller: 'sessions', action: 'new') unless session.include?(:name)
+    end
+
+
+
+end
+ 
