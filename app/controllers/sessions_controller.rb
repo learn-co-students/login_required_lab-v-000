64775ee4e0682b8efb3102d8a39f357923f2d142
Rachel Mandelbaum <rlmandelbaum@gmail.com>
2017-10-26 08:57:33 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+    render "application/login"
+  end
+
+  def create
+      if !params[:name].blank?
+      session[:name] = params[:name]
+      redirect_to "/secret"
+    else
+      redirect_to "/login"
+    end
+
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to "/login"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+
+  root 'application#index'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/secret' => 'secrets#show'
+  post '/logout' => 'sessions#destroy'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
