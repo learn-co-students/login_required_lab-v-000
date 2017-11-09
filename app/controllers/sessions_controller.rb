b5443875dd134205de8644d8e24a59f8ea6fad4a
Joanna <jmgsese@gmail.com>
2017-11-09 10:27:11 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    session[:name] = params[:name]
+    current_user = params[:name]
     if !params[:name] || params[:name] = ""
       redirect_to '/login'
     else
