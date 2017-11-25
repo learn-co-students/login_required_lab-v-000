7c18e8e1b3075ce91a069fa40391caa35ce67406
charlie  lee <ctlee9096@gmail.com>
2017-11-25 16:11:00 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
     ## calls name of user
     #The session method is available anywhere in the Rails
     #response cycle, and it behaves like a hash
+    #http://guides.rubyonrails.org/action_controller_overview.html#accessing-the-session
   end
 
 #   before_action :require_login
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,8 @@
 class SessionsController < ApplicationController
   def create
     session[:name] = params[:name]
-     if session[:name].present? #def present?!blank? end From this we can say that an object will return true if it is NOT blank.
+     if session[:name].present?
+       #def present?!blank? end From this we can say that an object will return true if it is NOT blank.
         #https://www.ruby-forum.com/topic/2306965
        redirect_to '/sessions/new'
      else
@@ -14,3 +15,4 @@ class SessionsController < ApplicationController
       redirect_to login_path
   end
 end
+#https://www.railstutorial.org/book/basic_login
