738a31d2faef07ab7760ebde9d38fbe209f757c5
EC2 Default User <ec2-user@ip-172-31-25-116.us-east-2.compute.internal>
2017-12-13 15:10:53 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  
+ 
+    def require_login
+        redirect_to controller: 'sessions', action: 'new' unless session.include? :name    
+    end
+    
+    def current_user
+      if session.include? :name
+        session[:name]
+      else
+        nil
+      end
+    end
 end
