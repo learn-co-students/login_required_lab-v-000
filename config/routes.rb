b62914c42c19f41656dc90042aa7f9015734b4ee
EC2 Default User <ec2-user@ip-10-0-0-95.ec2.internal>
2017-12-28 21:24:09 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root 'sessions#new'
+  get '/login' => 'sessions#new'
+  get '/welcome' => 'sessions#show'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+  get '/secret' => 'secrets#show'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
