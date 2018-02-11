31330251675e0d1f1a37a850d6195fd0e7b9920a
TravisA <github email address>
2018-02-10 23:10:53 -0700

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,5 @@
+class SecretsController < ApplicationController
+  def show
+  	redirect_to controller: 'sessions', action: 'new' unless session[:name]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'secrets/show'
+  
+  post '/destroy' => 'sessions#destroy'
+  resources :sessions
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
