f329ab0c51ebe67851b2c427096f9eaa55f386eb
mevanstron <michael@michaelgregoryevans.com>
2018-02-20 12:18:53 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+
+  resources :sessions, only: [:new, :create]
+
+  post '/destroy' => 'sessions#destroy'
+  get '/home' => 'sessions#home'
+  get '/secret' => 'secrets#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
