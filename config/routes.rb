f3cab682ac2f92cf3a4419b3e5699b352964c3fa
github username <github email address>
2018-02-21 12:28:48 +0200

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'application#homepage'
+   get '/login' => 'sessions#new'
+   post '/login' => 'sessions#create'
+   post '/logout' => 'sessions#destroy'
+   get 'secrets/show' => 'secrets#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
