591cc3cf87763915697bd10c5b5cd8c37ed9f18d
EC2 Default User <ec2-user@ip-172-31-38-157.us-east-2.compute.internal>
2018-03-07 20:29:27 +0000

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,12 @@
+class SecretsController < ApplicationController
+   before_action :require_login, only: [:show] 
+    
+   def show
+   end
+   
+   private 
+   
+   def require_login
+      redirect_to login_path unless current_user 
+   end
+end
