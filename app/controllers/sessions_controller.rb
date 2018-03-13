358b916160ed8ebb180a6403b4f8045bc609a216
An Nuyen <annuyen@gmail.com>
2018-03-13 18:24:10 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+
+  before_action :require_login, only: [:homepage]
+
+  def new
+  end
+
+  def create
+    if !params[:name] || params[:name].empty?
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      redirect_to homepage_path
+    end
+  end
+
+  def homepage
+    @user = session[:name]
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to login_path
+  end
+
+  private
+
+  def require_login
+    return head(:forbidden) unless session.include? :name
+  end
+end
