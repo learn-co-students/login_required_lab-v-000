bcf7e21f6bb1b24c990d734e85728e2522b37cd9
miriam <miriam5000@gmail.com>
2018-03-29 16:20:50 -0400

    tests passed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+root 'application#hello'
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+get 'link', to: 'secrets#show'
+post 'logout', to: 'sessions#destroy'
+
+
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
