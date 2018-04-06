780467fb196da87dbb2d0015a5cb7c2a6e199abf
jacobsilver2 <jacobsilver2@mac.com>
2018-04-06 08:40:18 -0400

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,17 +2,13 @@ class SecretsController < ApplicationController
   before_action :require_login
 
   def show
-    if !current_user
-      redirect_to '/sessions/new'
-    else
-      current_user
-    end
-  
   end
 
 
   private
+  #this method is defined in the application controller in the provided solution.  Not sure why, besides maybe that it's a pretty general method which could be used in more than one controller
   def require_login
-    return head(:forbidden) unless session.include? :name 
+    redirect_to '/sessions/new' unless current_user
   end
 end
+
