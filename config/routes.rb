947eb4562ed93ce95ac01211895095980b4749a7
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-04-11 11:19:53 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,10 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  root 'application#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+  get '/yoursecrets' => 'secrets#show'
 end
