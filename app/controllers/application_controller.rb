c524edaaddca040f6d2e81cb2b8e40dc9572ce78
funemployeddad <funemployeddad@gmail.com>
2018-04-12 15:41:13 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @user = User.find[params[:user_id]]
+    session[:name]
   end
 
+  def hello
+    
+  end
 end
--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,2 +1,15 @@
 class SecretsController < ApplicationController
+
+  def show
+    if require_login
+      render :secret
+    else
+      redirect_to login_path
+    end
+  end
+
+  private
+  def require_login
+    return head(:forbidden) unless session.include? :user_id
+  end
 end
