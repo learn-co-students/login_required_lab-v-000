3e235ca24eff8b435f34501ac659b819be17079f
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-04-13 00:05:40 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,23 @@ class SessionsController < ApplicationController
         session[:username] = params[:username]
         
     end
+    
+  def create
+   
+    if current_user.blank?
+    redirect_to '/sessions/new'
+    elsif current_user
+      redirect_to "/"
+    end
+  end
+  
+  def destroy
+    if session[:name] != nil
+    session.delete(:name)
+  else
+    session[:name] == nil
+    end
+    redirect_to root_path
+  end
 end
+
