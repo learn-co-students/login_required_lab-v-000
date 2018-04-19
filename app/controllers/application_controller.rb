5a33800011dab45f923171fd38743203beb9debd
Penguino <heriberto@melo.nyc>
2018-04-18 22:03:58 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,10 @@ class ApplicationController < ActionController::Base
   def current_user
     session[:name] ||= nil
   end
+
+  private
+
+  def require_logged_in
+      redirect_to controller: 'sessions', action: 'new' unless current_user
+  end
 end
--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,5 +1,6 @@
 class SecretsController < ApplicationController
+  before_action :require_logged_in
+
   def show
-      redirect_to controller: 'sessions', action: 'new' unless current_user
   end
 end
