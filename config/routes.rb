06a089e68dd8febbc98054d3ee28397668ad0151
ArielFeingold <feingold.ariel@gmail.com>
2018-04-27 16:33:55 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'secrets/new'
+  get 'secrets/show'
+
+  get 'sessions/new'
+  post 'sessions/create'
+  post 'sessions/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'sessions#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
