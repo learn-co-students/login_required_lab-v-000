f023dfd22abfd128a6357e507c84c23b7bfdb23b
Aubrey Neimeier <neimeier.1@osu.edu>
2018-06-05 16:55:05 +0000

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,5 +1,7 @@
 class SecretsController < ApplicationController
   before_action :require_logged_in
+
+  #require_logged_in saves us from all this logic in show
   # def show
   #   if current_user.nil?
   #     redirect_to login_path
