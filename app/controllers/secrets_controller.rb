eeb2f05c9cbb0c6607d513a636c131b80609e39f
Jonathan Tiede <jon@Jonathans-MacBook-Pro.local>
2018-06-06 17:31:51 -0600

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,10 +1,10 @@
 class SecretsController < ApplicationController
 	# before_action :require_login
 	def show
-		# if session[:name].blank?
-		# 	redirect_to '/login'
-		# end
-		redirect_to '/login' unless !session[:name]
+		if session[:name].blank?
+			redirect_to '/login'
+		end
+		# redirect_to '/login' unless !session[:name]
 	end
 
 	def index
