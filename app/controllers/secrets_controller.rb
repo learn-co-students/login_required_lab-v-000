762c700f6ead67cb6494d1c3a83eb0496f0cd256
github username <github email address>
2018-06-14 14:21:23 -0500

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,7 @@
+class SecretsController < ApplicationController
+  before_action
+#   make to prevent any route from being accessed without being logged in 
+    #       redirect_to login page
+    def index
+    end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def new
+    #where user will do login ... new page will be login page
+    # a user with invalid username should be redirected to the login page
+  
+  end
+
+
+
+  def show
+    # where the user will be welcomed
+  end
+end
