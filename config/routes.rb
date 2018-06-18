30e5f7e6f4948ec1e4d6b2b33ded1c56b9dd3ee9
github username <github email address>
2018-06-18 13:43:22 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+    resource :secrets
+    resource :sessions
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
