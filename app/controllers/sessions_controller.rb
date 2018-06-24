563d938278c4654bb0d7b3444894f17475f291f6
Carol Greene <carolgreene3@gmail.com>
2018-06-24 15:19:27 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  
+  def new
+  end
+
+  def create
+    session[:name] = params[:name]
+    if session[:name].blank?
+      redirect_to login_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    if session[:name].nil?
+      redirect_to root_path
+    else
+      session.delete :name
+      redirect_to root_path
+    end
+  end
+
+end
