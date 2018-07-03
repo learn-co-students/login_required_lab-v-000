cf45d6a6bd22f4b9b9c40f91ed4716329f78bc8d
kanndide <kanndide@gmail.com>
2018-07-03 17:43:54 +0300

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,9 +1,15 @@
 class SecretsController < ApplicationController
 
+	before_action :require_login
+
 	def show
-		if !current_user
-			redirect_to login_path
-		end
+		
+	end
+
+	private
+
+	def require_login
+		redirect_to login_path unless !!current_user
 	end
 
 end
