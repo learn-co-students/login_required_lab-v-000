046d36b739dc9f0cca60bb064822565517420990
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-07-11 18:48:29 -0400

    Implement login and logout actions in SessionsController

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+    def new
+        # just renders the form
+    end
+
+    def create
+        if params[:name] && !params[:name].empty?
+            session[:name] = params[:name]
+
+            redirect_to '/'
+        else
+            redirect_to action: :new
+        end
+    end
+
+    def destroy
+        session.delete("name") if session[:name]
+
+        redirect_to action: :new
+    end
+end
