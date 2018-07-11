01e6c80c7b47e14ff0bfc19e4011f5942621d9ad
AlexFriedland <friedlandaj@gmail.com>
2018-07-11 21:04:54 +0200

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+
+  def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to root_path
+    else
+      session[:name] = params[:name]
+      render :index
+    end
+  end
+
+    def destroy
+      session.clear
+      redirect_to '/'
+    end
+
+    def new
+    end
 end
