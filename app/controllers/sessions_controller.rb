cefbcf3e46c9179c6627c2162f677d4ddf8d925f
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-11 20:38:01 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+  def create
+    if params[:name] && !params[:name].empty?
+      session[:name] = params[:name]
+      redirect_to '/'
+    else
+      redirect_to '/sessions/new'
+    end
+  end
+
+  def destroy
+    if session[:name]
+      session.clear
+    end
+    redirect_to '/sessions/new'
+  end
+end
