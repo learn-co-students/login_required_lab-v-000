1c2cbf5627d7248fedecb6e21edc4017807ad575
Victoria Meng <nights1ststar@protonmail.com>
2018-07-26 16:32:34 +0000

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,13 +1,21 @@
 class SecretsController < ApplicationController
-  before_action :require_login
+  # skip_before_action :require_login, only: [:index, :login]
+
+  # def index
+  #
+  # end
+  #
+  # def login
+  #
+  #   redirect_to welcome_path
+  # end
+  #
+  # def welcome
+  #   @name = current_user
+  # end
 
   def show
-
   end
 
-  private
 
-  def require_login
-    return head(:forbidden) unless session.include?(:name)
-  end
 end
