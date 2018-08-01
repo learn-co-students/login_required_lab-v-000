6de71111c38f69925bd4cc2b0f9b62552ae2eb97
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-08-01 13:04:27 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if logged_in?
+      redirect_to '/sessions/new'
+    else
+      if !params[:name].nil? && params[:name].size > 0
+        session[:name] = params[:name]
+        redirect_to '/'
+      else
+        redirect_to '/sessions/new'
+      end
+    end
+  end
+
+  def destroy
+    session.delete :name
+    flash[:notice] = "You have logged out"
+    redirect_to '/'
+  end
+
+end
