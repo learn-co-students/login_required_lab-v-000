3cc1e448bfea4fc514f3fd8395b4bdd09e625b1e
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-03 17:53:39 +0000

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,3 @@
+class SecretssController < ApplicationController
+
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if !params[:name].present?
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+end
