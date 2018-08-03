d08ad0017ef37d1b77951a1b1088ad8d1d520cd1
taryn yaeger <tsyaeger@gmail.com>
2018-08-03 02:34:54 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,15 @@ class SessionsController < ApplicationController
    before_action :logged_in?
   
   def new
-
   end
 
   def create
-    session[:name] = params[:name] 
-    redirect_to sessions_show_path
+    if !params[:name].empty?
+      session[:name] = params[:name] 
+      redirect_to secrets_show_path
+    else
+      redirect_to root_path
+    end
   end
 
   def show
