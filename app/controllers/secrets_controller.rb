7e3c843ce2aec77130f439491a47bd89e06ab45c
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-06 19:06:28 +0000

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController 
+  before_action :require_login
+  
+  
+  private 
+  
+  def require_login
+   return head(:forbidden) unless session.include? :user_id    
+	end
+
+  
+end
