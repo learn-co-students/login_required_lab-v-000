fff3e49cf96208c6cb76e7b797af5cc2f4ffcc15
urnotjessie <zg2220@columbia.edu>
2018-08-10 10:58:15 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def create
+    if params[:name] == nil || params[:name] == ''
+      redirect_to '/login'
+    elsif session[:name]
+      redirect_to '/'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    if session[:name]
+      session.delete :name
+      redirect_to '/'
+    else
+      redirect_to '/'
+    end
+  end
+
+end
