503c164157932c841140652fb7b132475bb90140
Elaine Anderson <eparie@bu.edu>
2018-08-13 11:20:51 +0000

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,15 @@
+class SecretsController < ApplicationController
+  before_action :require_login
+
+def show
+end
+
+  def index
+  end
+
+  private
+
+  def require_login
+    redirect_to '/login' unless session.include? :user_id
+  end
+end
