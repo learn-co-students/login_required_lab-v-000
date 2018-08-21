b0c98005d72732f2b874fab0586451f3fa9aca9d
github username <github email address>
2018-08-21 18:57:56 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def index
+    unless session[:name]
+      redirect_to '/login'
+    end
+  end
+
   def current_user
     session[:name]
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,27 @@
 class SessionsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
+  def new
+
+  end
+
+  def create
+    if params[:name] == nil || params[:name] == ""
+      redirect_to '/login'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session[:name] = nil
+    redirect_to '/login'
+  end
+
+  private
+
+  def sessions_params
+    params.require(:session).permit(:name)
+  end
 end
