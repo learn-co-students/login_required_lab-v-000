26131e1cc83db0ffa1690ffd0f8241738e95cf81
Katrina Brinson <kbrinso@gmail.com>
2018-08-23 17:42:29 +0000

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,2 +1,17 @@
 class SecretsController < ApplicationController
+  # before_action :require_login
+
+  def show
+    if current_user.nil?
+      redirect_to '/sessions/new'
+    end
+  end
+
+  # private
+  # def require_login
+  #   def require_login
+  #     return head(:forbidden) unless session.include? :user_id
+  #   end
+  # end
+
 end
