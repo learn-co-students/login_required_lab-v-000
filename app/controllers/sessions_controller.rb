7f8f289365cd3e1caf59eb1c9df696c1df591897
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-08-27 22:20:14 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  before_action :require_login
+  
+  def new
+    current_user
+  end
+  
+  def create
+    if params[:name].blank?
+      redirect_to '/login'
+    else
+      redirect_to '/'
+    end
+  end
+  
+  def destroy
+    session.delete :name
+    redirect_to '/lgout'
+  end
+end
