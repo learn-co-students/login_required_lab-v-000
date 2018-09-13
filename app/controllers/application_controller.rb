ce6d1d2821c703e89aab169d2fad58fb13fb2d5d
JCesana <joshuacesana@gmail.com>
2018-09-13 14:23:26 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @current_user ||= session[:name]
+    session[:name]
   end
 
+  private
+
+  def require_login
+    redirect_to controller: 'sessions', action: 'new' unless current_user
+  end
 end
--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,15 +2,5 @@ class SecretsController < ApplicationController
   before_action :require_login
 
   def show
-    @secret = "French Bulldogs are the best!"
-  end
-
-  private
-
-  def require_login
-    if !session.include? :name
-      flash[:error] = "You must be logged in to access this section"
-      redirect_to '/'
-    end
   end
 end
