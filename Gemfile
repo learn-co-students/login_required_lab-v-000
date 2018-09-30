6f609804d31f1af12a3be166c2a130cb5cd67cbb
Richard-Frost <frost.rchrd@gmail.com>
2018-09-30 10:25:52 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -43,4 +43,6 @@ group :development, :test do
   gem 'spring'
   gem 'rspec-rails', '~> 3.7'
   gem 'capybara'
+  gem 'pry'
+  gem 'rb-readline'
 end
---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,10 @@
+class SecretsController < ApplicationController
+  before_action :require_login
+  
+  def create 
+  end
+
+  def show
+  end
+
+end
