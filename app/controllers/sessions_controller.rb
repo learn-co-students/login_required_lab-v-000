7e763f2cc638f6e070ce2f04a652a2890d903e69
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-10-03 17:41:43 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,33 @@
 class SessionsController < ApplicationController
+
+  def show
+    require_login
+  end
+
+  def new
+  end
+
+  def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to '/login'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    if !current_user.nil?
+      session[:name] = nil
+      redirect_to '/login'
+    else
+      redirect_to '/'
+    end
+  end
+
+  private
+
+  def require_login
+    redirect_to '/login' unless session.include? :name
+  end
 end
