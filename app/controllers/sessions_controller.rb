b5098f45178f30706b950ae127026b626ff7c1c3
Everett <everett.bowline@gmail.com>
2018-10-14 18:54:35 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,32 +1,23 @@
-require 'pry'
-
 class SessionsController < ApplicationController
-  before_action :require_login
 
   def new
   end
 
   def create
-    binding.pry
     if !params[:name].present?
       redirect_to root_path
     else
       session[:name] = params[:name]
+      redirect_to 'secrets/show'
     end
   end
 
   def destroy
-    if session[:name]
+    if !session[:name] == nil
       session.delete :name
     else
       session[:name] = nil
     end
   end
-
-  private
-
-    def require_login
-      redirect_to controller: 'sessions', action: 'new' unless logged_in?
-    end
 
 end
