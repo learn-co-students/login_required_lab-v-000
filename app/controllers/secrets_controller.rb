aea9e7e6a566fb09c8e6355bc7c41ee6b43eda04
epic0448 <samuelogundiran@Yahoo.com>
2018-10-19 08:13:26 -0400

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,13 +1,10 @@
 class SecretsController < ApplicationController
-    before_action :require_login
+     before_action :require_login
       
-    #  note: the session exist for us already.... futher in the lesson we will have to code for it
+    # #  note: the session exist for us already.... futher in the lesson we will have to code for it
     
-    def show
-      
-    end
 
-    private
+     private
 
     def require_login
           redirect_to root_path unless current_user      
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,5 @@
 class SessionsController < ApplicationController
 
-    def new
-    end
-  
-
 
     def create
         if params[:name].nil? || params[:name].empty?
