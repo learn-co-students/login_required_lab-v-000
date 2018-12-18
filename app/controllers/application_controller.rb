4595b0e0d1b3c280d9a9f8e581c6b2816d1cd65a
github username <github email address>
2018-12-17 18:45:31 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,20 @@ class ApplicationController < ActionController::Base
   def hello
     @user = current_user
   end
+
+  # *** Solution ***
+  
+  # def hello
+  #   redirect_to controller: 'sessions', action: 'new' unless session[:name]
+  # end
+  #
+  # def current_user
+  #   session[:name]
+  # end
+  #
+  # private
+  #
+  # def require_logged_in
+  #   redirect_to controller: 'sessions', action: 'new' unless current_user
+  # end
 end
