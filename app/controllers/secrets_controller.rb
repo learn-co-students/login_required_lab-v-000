5beeb9983a09b992e246f2cf40cec5777669f01a
github username <github email address>
2019-01-03 12:18:44 -0500

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,15 @@
+require 'pry'
+class SecretsController < ApplicationController
+  before_action :require_login
+
+  def show
+
+  end
+
+private
+
+  def require_login
+    redirect_to '/sessions/new' unless logged_in?
+  end
+
+end
