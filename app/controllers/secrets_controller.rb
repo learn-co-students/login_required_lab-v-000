e59c9848a1f0cdf0340a18e67a719c020c6e8026
ConorHamilton19 <conhamil19@gmail.com>
2019-01-17 14:14:14 -0600

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,20 @@
+class SecretsController < ApplicationController
+
+  before_action :require_login
+
+  def show
+  end
+
+
+  private
+
+  def require_login
+    if current_user
+      render 'secrets/show'
+    else
+      redirect_to '/login'
+    end
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
+  root 'application#home'
+  get '/secret', to: 'secrets#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
