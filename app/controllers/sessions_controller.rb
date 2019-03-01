6a40783ce32173d233eabe49e7ebf7c08a95c749
Melissa Real <melissa.real@hotmail.com>
2019-03-01 14:29:34 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def index
+  end
+
+  def create
+    if !params[:name] || params[:name]== "" || params[:name].nil?
+      redirect_to login_path
+    else
+     session[:name] = params[:name]
+     redirect_to root_path
+    end
+
+  end
+
+  def destroy
+    session.destroy
+    redirect_to login_path
+  end
+
+
+end
