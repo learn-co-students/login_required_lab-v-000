228df58bc2fe94f25a40b2fc8a87cb27caefa26a
Erin Green <erin.y.green@gmail.com>
2019-03-20 02:01:03 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,10 @@ class SessionsController < ApplicationController
   end
 
   def create
+    return redirect_to(controller: 'sessions', action: 'new') if !params[:name] || params[:name].empty?
     session[:name] = params[:name]
 
-    redirect_to(controller: 'sessions', action: 'new')
+    redirect_to(controller: 'application', action: 'homepage')
   end
 
   def destroy
