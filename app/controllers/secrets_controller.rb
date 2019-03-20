17b3403ec255a76c222e2485c9556c6aff4329af
Erin Green <erin.y.green@gmail.com>
2019-03-20 01:45:46 +0000

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,8 +1,8 @@
 class SecretsController < ApplicationController
   before_action :current_user
 
-  def favorite_color
-    render controller: secrets, action:
+  def show
+    
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'sessions#new'
-  root '/homepage'
+  get 'secrets/new'
+
+  root 'application#homepage'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 
-  get '/secret' => 'secret#favorite_color'
+  get '/secret' => 'secrets#show'
 end
