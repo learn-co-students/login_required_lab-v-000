f08e9cbca91fae63964a05390aa95a817c406459
Carley Tripp <carley.tripp@outlook.com>
2019-04-04 21:54:56 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'application#welcome'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
+  get 'secrets/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
