f736215e676eb7fe4fd78d0c61aa1033b031605e
Ashley <ashleywong678@gmail.com>
2019-04-24 19:26:20 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,29 @@
 class SessionsController < ApplicationController
 
   def show
+    
   end
+
+  # describe 'create' do
+  #   it 'redirects to login page if :name is nil' do
+  #     post :create, params: { name: nil }
+  #     expect(response).to redirect_to controller: 'sessions', action: 'new'
+  #   end
+
+
 end
+
+
+
+
+#   it 'redirects to login page if :name is empty' do
+#     post :create, params: { name: '' }
+#     expect(response).to redirect_to controller: 'sessions', action: 'new'
+#   end
+
+#   it 'sets session[:name] if :name was given' do
+#     me = 'Werner Brandes'
+#     post :create, params: { name: me }
+#     expect(@request.session[:name]).to eq me
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/show', to: 'sessions#show'
 
+  resources :sessions, only: [:show, :new]
+  resources :secrets, only: [:show]
 end
