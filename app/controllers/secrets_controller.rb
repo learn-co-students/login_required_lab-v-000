7f00384b40614b4052ad982359b4baacc460951d
Heather Miller <heather.miller114@gmail.com>
2019-04-30 16:25:34 -0400

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,19 @@
+class SecretsController < ApplicationController
+    before_action :require_login
+    skip_before_action :require_login, only: [:index]
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    private
+    
+    def require_login
+        return head(:forbidden) unless session.include? :user_id
+    end
+
+end
