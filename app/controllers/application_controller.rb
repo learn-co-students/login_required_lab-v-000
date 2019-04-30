83666fb42cb41edbbae33367df03af5b638d7c65
sarawall1187 <sarawall1187@gmail.com>
2019-04-30 10:18:05 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
+require 'pry'
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  def current_user
+   if params[:name] != nil 
+    session[:name] = params[:name]
+   else
+    redirect_to :sessions
+   end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'secrets/new'
+
+  resources :sessions, only: [:index, :new, :create]
+
+  # get 'sessions/new'
+
+  # get 'sessions/index'
+  # post '/sessions'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
