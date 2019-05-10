555118e1af5f0c5880aa16d07ca98e345abc8675
github username <github email address>
2019-05-10 09:48:00 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def hello
+
+  end
+
   def current_user
     if !!session[:name]
-      session[:name]
+        session[:name]
     end
-    
+
   end
+
 end
