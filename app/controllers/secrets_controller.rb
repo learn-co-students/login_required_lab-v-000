61ebcba6a050cd5be471d624be94bb80d7a02c4e
github username <github email address>
2019-05-10 14:17:27 -0400

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,6 +1,6 @@
 class SecretsController < ApplicationController
     before_action :require_login
-    skip_before_action :require_login, only: [:show]
+#    skip_before_action :require_login, only: [:show]
 
   def show
     if !session[:name]
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    raise params.inspect
+    #raise params.inspect
     if params[:name] == nil || params[:name] == ""
       redirect_to login_path
     else
       session[:name] = params[:name]
-      redirect_to login_path
+      redirect_to root_path
     end
   end
 
   def destroy
     session.delete :name
+    redirect_to controller: "application", action: "hello"
   end
 end
