8699b81354f4c6a680238ec81f187d1b57a6378f
Meg Gutshall <meghan.gutshall@gmail.com>
2019-06-03 17:43:35 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 class SessionsController < ApplicationController
   def new
-    
+
+  end
+
+  def create
+    session[:name] = params[:name]
+    if session[:name].nil? || session[:name].empty?
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.delete :name
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/secret', to: 'secrets#show'
+  get '/', to: 'secrets#show'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/login', to: 'sessions#destroy'
 end
