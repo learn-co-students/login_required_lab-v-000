a799bf08daa9b6b3cf05e8fb38263da7473c542d
Tom Cho <tc.thomascho@gmail.com>
2019-06-28 10:23:59 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to login_path
+    elsif params[:name]
+      session[:name] = params[:name]
+      redirect_to root_path
+    end
+
+  end
+
+  def destroy
+    session.destroy if current_user
+    redirect_to login_path
+    
+  end
+
+end
