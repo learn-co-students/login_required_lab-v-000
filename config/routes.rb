adbaa186632283c645030c621c0f7aa26a53fc5c
github username <github email address>
2019-08-21 02:38:06 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'application#home'
+    get 'secrets/new'
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    post '/logout' => 'sessions#destroy'
+
+    get '/secrets' => 'secrets#show'
+
 end
