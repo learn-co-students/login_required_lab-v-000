d223f9e9d23f102d7b9c54d7034b701bac46cea2
Thomas Gray <thomas.gray@tgray.fios-router.home>
2019-09-13 09:04:11 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'sessions#new'
+
+  post '/sessions/destroy', to: 'sessions#destroy'
+  resources :sessions
+  get '/secret', to: 'secrets#show'
+
+
 end
