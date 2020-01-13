734ed3628cbc74d97e5e181870f0bcdcae0ca7a6
sdcro <sdcrouse91@gmail.com>
2020-01-13 01:38:06 -0800

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,4 +1,5 @@
 class SecretsController < ApplicationController
+  # This all works, but I want to refactor this stuff with #before_action (and in other ways if possible).
   def show
     if current_user
       render :show
