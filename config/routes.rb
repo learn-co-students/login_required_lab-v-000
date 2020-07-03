06876345a1b9cee80f9cd3f10d78c6298a7f7ecd
Sheena Bonilla <sheena.myb@gmail.com>
2020-07-03 03:20:54 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'application#hello'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
