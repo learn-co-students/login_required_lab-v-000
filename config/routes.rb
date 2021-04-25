79a738acf4d31a94b9c27056afd1255f057dc034
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-04-24 20:27:29 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # this appeared in the previous lab as well, the related action redirects to sessions#new
+  root 'application#hello'
+  get '/login', to: 'sessions#new'
+  get '/home', to: 'sessions#home'
+  get '/secrets', to: 'secrets#show'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
 end
